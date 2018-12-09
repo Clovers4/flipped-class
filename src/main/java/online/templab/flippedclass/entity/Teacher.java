92f88670474f9201adee3d525ab5ca3879097c90
wk <wkaywork@163.com>
2018-12-09 13:28:48 +0800

    [test] 测试好通用Mapper的使用，确定可以满足迭代集成要求

--- src/main/java/online/templab/flippedclass/entity/Teacher.java
+++ src/main/java/online/templab/flippedclass/entity/Teacher.java
@@ -1,5 +1,34 @@
 package online.templab.flippedclass.entity;
 
-// TODO:待补充
-public class Teacher {
+import java.io.Serializable;
+import javax.persistence.*;
+
+import lombok.Data;
+import lombok.experimental.Accessors;
+
+@Data
+@Accessors(chain = true)
+@Table(name = "`teacher`")
+public class Teacher implements Serializable {
+    @Id
+    @Column(name = "`id`")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(name = "`account`")
+    private String account;
+
+    @Column(name = "`password`")
+    private String password;
+
+    @Column(name = "`name`")
+    private String name;
+
+    @Column(name = "`email`")
+    private String email;
+
+    @Column(name = "`activative`")
+    private Boolean activative;
+
+    private static final long serialVersionUID = 1L;
 }
