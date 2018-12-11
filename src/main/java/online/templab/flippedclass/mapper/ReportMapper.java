ff1a4cba854589eee4883aee94591195dc368f6a
wk <wkaywork@163.com>
2018-12-11 22:40:13 +0800

    [refactor] 去除不必要的方法，补充一个遗漏注释

--- src/main/java/online/templab/flippedclass/mapper/ReportMapper.java
+++ src/main/java/online/templab/flippedclass/mapper/ReportMapper.java
@@ -4,6 +4,9 @@ import online.templab.flippedclass.entity.Report;
 import org.springframework.stereotype.Component;
 import tk.mybatis.mapper.common.Mapper;
 
+/**
+ * @author wk
+ */
 @Component
 public interface ReportMapper extends Mapper<Report> {
 }
--- src/main/java/online/templab/flippedclass/mapper/TeacherMapper.java
+++ src/main/java/online/templab/flippedclass/mapper/TeacherMapper.java
@@ -4,14 +4,9 @@ import online.templab.flippedclass.entity.Teacher;
 import org.springframework.stereotype.Component;
 import tk.mybatis.mapper.common.Mapper;
 
+/**
+ * @author wk
+ */
 @Component
 public interface TeacherMapper extends Mapper<Teacher> {
-
-    /**
-     * 通过账号查询密码
-     *
-     * @param account
-     * @return
-     */
-    String getPassword(String account);
 }
