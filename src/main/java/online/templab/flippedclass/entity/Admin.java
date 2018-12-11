abb0431a0f0fd509faa3e58693e0bf453a747b98
wk <wkaywork@163.com>
2018-12-11 22:39:12 +0800

    [feat] 新增用户相关的实体类、Mapper、xml

---
+++ src/main/java/online/templab/flippedclass/entity/Admin.java
@@ -0,0 +1,28 @@
+package online.templab.flippedclass.entity;
+
+import java.io.Serializable;
+import javax.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+import lombok.experimental.Accessors;
+
+@Getter
+@Setter
+@ToString
+@Accessors(chain = true)
+@Table(name = "`admin`")
+public class Admin implements Serializable {
+    @Id
+    @Column(name = "`id`")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(name = "`account`")
+    private String account;
+
+    @Column(name = "`password`")
+    private String password;
+
+    private static final long serialVersionUID = 1L;
+}
