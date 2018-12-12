f9aa09a05ca2cb9d05a6be4ea53c90cf00185b0b
wk <wkaywork@163.com>
2018-12-12 13:36:04 +0800

    [fix] 允许EmailService抛出异常，而不是直接拦截掉MailException

--- src/main/java/online/templab/flippedclass/service/EmailService.java
+++ src/main/java/online/templab/flippedclass/service/EmailService.java
@@ -3,6 +3,7 @@ package online.templab.flippedclass.service;
 import online.templab.flippedclass.entity.Email;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.mail.MailException;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
@@ -23,18 +24,14 @@ public class EmailService {
     @Value("${spring.mail.username}")
     private String username;
 
-    public void send(Email email) {
+    public void send(Email email) throws MailException {
         SimpleMailMessage message = new SimpleMailMessage();
         message.setFrom(username);
         message.setTo(email.getToAddress());
         message.setSubject(email.getSubject());
         message.setText(email.getContent());
-        try {
-            mailSender.send(message);
-            // TODO: 改成logger
-            System.out.println("简单邮件发送成功！");
-        } catch (Exception e) {
-            System.out.println("发送简单邮件时发生异常！" + e);
-        }
+        mailSender.send(message);
+        // TODO: 改成logger
+        System.out.println("简单邮件发送成功！");
     }
 }
