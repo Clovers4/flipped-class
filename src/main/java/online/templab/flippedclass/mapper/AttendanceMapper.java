82d8156d6178580be958174ccd5b857918205780
wk <wkaywork@163.com>
2018-12-16 20:45:11 +0800

    [fix] 所有Mapper前加上@Component与author信息

--- src/main/java/online/templab/flippedclass/mapper/AttendanceMapper.java
+++ src/main/java/online/templab/flippedclass/mapper/AttendanceMapper.java
@@ -4,6 +4,9 @@ import online.templab.flippedclass.entity.Attendance;
 import org.springframework.stereotype.Component;
 import tk.mybatis.mapper.common.Mapper;
 
+/**
+ * @author wk
+ */
 @Component
 public interface AttendanceMapper extends Mapper<Attendance> {
 }
--- src/main/java/online/templab/flippedclass/mapper/CourseMapper.java
+++ src/main/java/online/templab/flippedclass/mapper/CourseMapper.java
@@ -1,7 +1,12 @@
 package online.templab.flippedclass.mapper;
 
 import online.templab.flippedclass.entity.Course;
+import org.springframework.stereotype.Component;
 import tk.mybatis.mapper.common.Mapper;
 
+/**
+ * @author wk
+ */
+@Component
 public interface CourseMapper extends Mapper<Course> {
 }
