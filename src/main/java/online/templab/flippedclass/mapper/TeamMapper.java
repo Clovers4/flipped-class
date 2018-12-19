8e22cf9502cb851e4ae4680c9ebfdd8705684c64
fj <1326372778@qq.com>
2018-12-19 21:44:50 +0800

    [feat] 新增TeamService 中与组队相关部分（缺少队伍合法性验证）以及team相关mapper和dao

--- src/main/java/online/templab/flippedclass/mapper/TeamMapper.java
+++ src/main/java/online/templab/flippedclass/mapper/TeamMapper.java
@@ -1,12 +1,24 @@
 package online.templab.flippedclass.mapper;
 
+import online.templab.flippedclass.entity.Student;
 import online.templab.flippedclass.entity.Team;
 import org.springframework.stereotype.Component;
 import tk.mybatis.mapper.common.Mapper;
+
+import java.util.List;
 
 /**
  * @author wk
  */
 @Component
 public interface TeamMapper extends Mapper<Team> {
+
+    /**
+     * 学生退组
+     *
+     * @param teamId
+     * @param studentId
+     * @return
+     */
+    Boolean deleteMember(Long teamId,Long studentId);
 }
