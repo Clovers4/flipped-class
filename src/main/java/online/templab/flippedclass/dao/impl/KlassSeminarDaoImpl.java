77e1832eb7f5b12d533425f32e4c42860a1ec5c9
lighter47 <975807232@qq.com>
2018-12-22 13:51:44 +0800

    [feat] 实现了klassSeminar的增加、删除、查看，并添加测试

--- src/main/java/online/templab/flippedclass/dao/impl/KlassSeminarDaoImpl.java
+++ src/main/java/online/templab/flippedclass/dao/impl/KlassSeminarDaoImpl.java
@@ -1,8 +1,12 @@
 package online.templab.flippedclass.dao.impl;
 
 import online.templab.flippedclass.dao.KlassSeminarDao;
+import online.templab.flippedclass.entity.Attendance;
 import online.templab.flippedclass.entity.KlassSeminar;
+import online.templab.flippedclass.entity.Seminar;
+import online.templab.flippedclass.mapper.AttendanceMapper;
 import online.templab.flippedclass.mapper.KlassSeminarMapper;
+import online.templab.flippedclass.mapper.SeminarMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
@@ -15,9 +19,29 @@ public class KlassSeminarDaoImpl implements KlassSeminarDao {
     @Autowired
     KlassSeminarMapper klassSeminarMapper;
 
+    @Autowired
+    SeminarMapper seminarMapper;
+
+    @Autowired
+    AttendanceMapper attendanceMapper;
+
     @Override
     public Boolean insert(KlassSeminar klassSeminar) {
         int line = klassSeminarMapper.insert(klassSeminar);
         return line == 1;
     }
+
+    @Override
+    public KlassSeminar selectOne(Long klassId, Long seminarId) {
+        return klassSeminarMapper.selectOneByKlassIdSeminarId(klassId,seminarId);
+
+    }
+
+    @Override
+    public Boolean delete(Long id) {
+        attendanceMapper.delete(new Attendance().setKlassSeminarId(id));
+        return klassSeminarMapper.deleteByPrimaryKey(id)==1;
+    }
+
+
 }
--- src/test/java/online/templab/flippedclass/service/impl/SeminarServiceImplTest.java
+++ src/test/java/online/templab/flippedclass/service/impl/SeminarServiceImplTest.java
@@ -1,9 +1,11 @@
 package online.templab.flippedclass.service.impl;
 
 import online.templab.flippedclass.FlippedClassApplicationTest;
-import online.templab.flippedclass.entity.KlassSeminar;
-import online.templab.flippedclass.entity.Seminar;
-import online.templab.flippedclass.service.KlassSeminarService;
+import online.templab.flippedclass.entity.*;
+import online.templab.flippedclass.mapper.AttendanceMapper;
+import online.templab.flippedclass.mapper.KlassSeminarMapper;
+import online.templab.flippedclass.service.CourseService;
+import online.templab.flippedclass.service.KlassService;
 import online.templab.flippedclass.service.SeminarService;
 import org.junit.Assert;
 import org.junit.Test;
@@ -15,6 +17,7 @@ import java.util.Date;
 
 /**
  * @author fj
+ * @author jh
  */
 @Transactional
 public class SeminarServiceImplTest extends FlippedClassApplicationTest {
@@ -23,32 +26,72 @@ public class SeminarServiceImplTest extends FlippedClassApplicationTest {
     SeminarService seminarService;
 
     @Autowired
-    KlassSeminarService klassSeminarService;
+    KlassService klassService;
 
-    private Seminar createSeminar(){
+    @Autowired
+    CourseService courseService;
+
+    @Autowired
+    AttendanceMapper attendanceMapper;
+
+    @Autowired
+    KlassSeminarMapper klassSeminarMapper;
+
+    private void createKlassSeminarTable() {
+        courseService.insert(new Course()
+                .setId((long) 123)
+                .setTeamEndDate(new Date())
+                .setTeamStartDate(new Date())
+                .setReportPercentage(30)
+                .setQuesPercentage(20)
+                .setPrePercentage(50)
+                .setTeacherId((long) 1)
+                .setIntroduction("1")
+                .setCourseName("1"));
+        klassService.insert(new Klass()
+                .setCourseId((long) 123)
+                .setId((long) 213)
+                .setGrade(3)
+                .setLocation("")
+                .setSerial(1)
+                .setTime(""));
+        seminarService.insert(new Seminar()
+                .setContent("")
+                .setCourseId((long) 123)
+                .setEnrollEndDate(new Date())
+                .setId((long) 12321)
+                .setMaxTeam(6)
+                .setEnrollStartDate(new Date())
+                .setRoundId((long) 123)
+                .setSerial(3)
+                .setVisible(true)
+                .setTheme("主题"));
+    }
+
+    private Seminar createSeminar() {
         return new Seminar()
-                .setCourseId((long)random.nextInt(10))
+                .setCourseId((long) random.nextInt(10))
                 .setContent("testContent")
                 .setEnrollEndDate(new Date())
                 .setEnrollStartDate(new Date())
                 .setMaxTeam(random.nextInt(10))
-                .setRoundId((long)random.nextInt(10))
+                .setRoundId((long) random.nextInt(10))
                 .setSerial(random.nextInt(20))
                 .setTheme("testTheme")
                 .setVisible(true);
     }
 
     @Test
-    public void testInsert()throws Exception{
+    public void testInsert() throws Exception {
         Seminar seminar = createSeminar();
         logger.info(seminar.toString());
         Boolean success = seminarService.insert(seminar);
         logger.info(seminar.toString());
-        Assert.assertEquals(true,success);
+        Assert.assertEquals(true, success);
     }
 
     @Test
-    public void testUpdate()throws Exception{
+    public void testUpdate() throws Exception {
         Seminar seminar = createSeminar();
         seminarService.insert(seminar);
         logger.info(seminar.toString());
@@ -59,47 +102,81 @@ public class SeminarServiceImplTest extends FlippedClassApplicationTest {
         Boolean success = seminarService.update(seminar);
         logger.info(seminar.toString());
         logger.info(success.toString());
-        Assert.assertEquals(true,success);
+        Assert.assertEquals(true, success);
     }
 
     @Test
-    public void testDelete()throws Exception{
+    public void testDelete() throws Exception {
         Seminar seminar = createSeminar();
         seminarService.insert(seminar);
         logger.info(seminar.toString());
         KlassSeminar klassSeminar = new KlassSeminar()
                 .setSeminarId(seminar.getId())
-                .setKlassId((long)random.nextInt(3))
+                .setKlassId((long) random.nextInt(3))
                 .setState(1)
                 .setReportDeadline(new Date());
-        klassSeminarService.insert(klassSeminar);
+        seminarService.insertKlassSeminar(klassSeminar);
         logger.info(klassSeminar.toString());
         Boolean success = seminarService.delete(seminar.getId());
         logger.info(success.toString());
-        Assert.assertEquals(true,success);
+        Assert.assertEquals(true, success);
     }
 
     @Test
-    public void testGet()throws Exception{
+    public void testGet() throws Exception {
         Seminar seminar = createSeminar();
         seminarService.insert(seminar);
         logger.info(seminar.toString());
         Seminar recordSeminar = seminarService.get(seminar.getId());
         logger.info(recordSeminar.toString());
-        Assert.assertEquals(recordSeminar.getId(),seminar.getId());
+        Assert.assertEquals(recordSeminar.getId(), seminar.getId());
     }
 
     @Test
-    public void testGetMaxSeminarSerialByCourseId()throws Exception{
-        Seminar seminars [] = new Seminar[5];
-        for(int i=0;i<5;i++){
+    public void testGetMaxSeminarSerialByCourseId() throws Exception {
+        Seminar seminars[] = new Seminar[5];
+        for (int i = 0; i < 5; i++) {
             seminars[i] = createSeminar();
-            seminars[i].setCourseId((long)2);
+            seminars[i].setCourseId((long) 2);
             seminarService.insert(seminars[i]);
         }
         logger.info(seminars.toString());
-        Integer maxSeminarSerial = seminarService.getMaxSeminarSerialByCourseId((long)2);
+        Integer maxSeminarSerial = seminarService.getMaxSeminarSerialByCourseId((long) 2);
         logger.info(maxSeminarSerial.toString());
         Assert.assertNotNull(maxSeminarSerial);
+    }
+
+    @Test
+    public void testGetKlassSeminar() {
+        createKlassSeminarTable();
+        attendanceMapper.insertSelective(new Attendance()
+                .setKlassSeminarId((long) klassSeminarMapper.selectOneByKlassIdSeminarId((long) 213, (long) 12321).getId())
+                .setTeamId((long) 1)
+                .setPresent(true)
+                .setPptName("pptname")
+                .setPreFile("ppturl")
+                .setReportName("reportname")
+                .setReportFile("reporturl")
+                .setSn(3));
+        KlassSeminar klassSeminar = seminarService.getKlassSeminar((long) 213, (long) 12321);
+        logger.info(klassSeminar.toString());
+        Assert.assertEquals((long) klassSeminar.getSeminarId(), (long) 12321);
+    }
+
+    @Test
+    public void testDeleteKlassSeminar() {
+        createKlassSeminarTable();
+        attendanceMapper.insertSelective(new Attendance()
+                .setKlassSeminarId((long) klassSeminarMapper
+                        .selectOneByKlassIdSeminarId((long) 213, (long) 12321).getId())
+                .setTeamId((long) 1)
+                .setPresent(true)
+                .setPptName("pptname")
+                .setPreFile("ppturl")
+                .setReportName("reportname")
+                .setReportFile("reporturl")
+                .setSn(3));
+        seminarService.deleteKlassSeminar((long) klassSeminarMapper
+                .selectOneByKlassIdSeminarId((long) 213, (long) 12321).getId());
     }
 }
