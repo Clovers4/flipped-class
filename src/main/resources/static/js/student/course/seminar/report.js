2b7368663bec1f57c03d6b70b8e83a5eac6dbac0
wk <wkaywork@163.com>
2018-12-26 19:58:40 +0800

    [feat] 引入前端和TeacherController

--- src/main/resources/static/js/student/course/seminar/report.js
+++ src/main/resources/static/js/student/course/seminar/report.js
@@ -1,62 +1,38 @@
 var file;
 var uploadName;
-var teamPPT;
-var downloadFileForm;
-var fileNameInput;
+var teamReport;
 $(function () {
     file = $("#fileInput");
     uploadName = $("#uploadName");
-    teamPPT = $("#teamPPT");
-    downloadFileForm = $("#downloadFileForm");
-    fileNameInput = $("#fileNameInput");
-
-    var uploadLi = $("#uploadLi");
-    if(uploadLi.length > 0){
-        $("#attendanceId").val(uploadLi.attr("data-atdId"));
-    }
+    teamReport = $("#teamReport");
 
     $("#returnCourseId").val(sessionStorage.getItem("courseId"));
     $("#backBtn").click(function () {
         $("#returnForm").submit();
     });
-    $(".enroll").click(function () {
-        $("#snInput").val($(this).attr("data-idx"));
-        $.ajax({
-            type: "post",
-            url: "/student/course/seminar/enroll",
-            data: $("#enrollForm").serialize(),
-            success: function () {
-                window.location.reload();
-            },
-            error: function () {
-                util.showAlert("danger", "报名失败，已报名或该位置已被报名", 3)
-            }
-        })
-    });
     $("#upload").click(function () {
-        console.log(file);
         file.trigger("click");
     });
     file.on("input propertychange", function () {
         var files = file.get(0).files;
+        console.log(files);
         if(files.length > 0) {
             uploadName.html(files[0].name);
         }
     });
 
     $("#confirmUpload").click(function () {
-        var verify = util.verifyWithAlert(teamPPT);
+        var verify = util.verifyWithAlert(teamReport);
         if(verify == null){
             util.showLoading();
             $.ajax({
                 type:"post",
-                url:"/student/course/seminar/uploadPPT",
+                url:"/student/course/seminar/uploadReport",
                 contentType: false,
                 processData: false,
-                data: new FormData(teamPPT.get(0)),
+                data: new FormData(teamReport.get(0)),
                 success:function(){
                     util.hideLoading();
-                    $("#pptModal").modal("hide");
                     util.showAlert("success", "上传成功", 3);
                 },
                 error:function () {
@@ -66,9 +42,4 @@ $(function () {
             })
         }
     });
-    $(".download-ppt").click(function () {
-        console.log(this);
-        fileNameInput.val('1-class.xlsx');
-        downloadFileForm.submit();
-    })
 });
